386309d4024ca67a9ddd02cc9600e9735e56c2a7
Shane Witbeck <shane.witbeck@nike.com>
2023-06-28 11:19:00 -0700

    feat: add supabase client, add superforms

---
+++ src/lib/Supabase.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { createClient } from '@supabase/supabase-js'
+    export const supabase = createClient('https://ablxiptxuxppiusdcjcc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFibHhpcHR4dXhwcGl1c2RjamNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc4MTgyMDMsImV4cCI6MjAwMzM5NDIwM30.eJsIwq5OEYXkDgHVIkjPVQw_wL7krB09aNx25hcajnM')
+</script>
+
